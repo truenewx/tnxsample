3b8bff1baa3a914a6f15584f80b7e22c8de2ae41
jiangnanyuzi <jiangnanyuzi@163.com>
2020-03-09 22:30:01 +0800

    改进Feign接口及实现方式

--- tnxsample-facade/tnxsample-facade-api/src/main/java/org/truenewx/tnxsample/facade/api/UserApi.java
+++ tnxsample-facade/tnxsample-facade-api/src/main/java/org/truenewx/tnxsample/facade/api/UserApi.java
@@ -1,6 +1,5 @@
 package org.truenewx.tnxsample.facade.api;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +10,6 @@ import org.truenewx.tnxsample.facade.model.entity.User;
  *
  * @author jianglei
  */
-@FeignClient(name = "ms-facade", url = "localhost:8080")
 @RequestMapping("/user")
 public interface UserApi {
 
