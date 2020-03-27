c6b12592e4db468928054acffaedc402f4b43321
jiangnanyuzi <jiangnanyuzi@163.com>
2020-03-27 17:19:23 +0800

    修正bug

--- tnxsample-admin/tnxsample-admin-service/src/main/java/org/truenewx/tnxsample/admin/service/model/CommandManagerSelf.java
+++ tnxsample-admin/tnxsample-admin-service/src/main/java/org/truenewx/tnxsample/admin/service/model/CommandManagerSelf.java
@@ -7,7 +7,7 @@ import org.truenewx.tnxjee.model.validation.constraint.NotContains;
 import org.truenewx.tnxsample.admin.model.entity.Manager;
 
 /**
- * 管理自身的命令模型
+ * 管理员自身的命令模型
  */
 @Getter
 @Setter
--- tnxsample-admin/tnxsample-admin-web/src/main/java/org/truenewx/tnxsample/admin/web/controller/MineController.java
+++ tnxsample-admin/tnxsample-admin-web/src/main/java/org/truenewx/tnxsample/admin/web/controller/MineController.java
@@ -4,10 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.truenewx.tnxjee.web.security.config.annotation.ConfigAuthority;
 import org.truenewx.tnxsample.admin.service.ManagerService;
@@ -34,6 +31,7 @@ public class MineController {
     @ApiOperation("修改个人信息")
     @PostMapping("/info")
     @ConfigAuthority
+    @ResponseBody
     public void updateInfo(@Valid @RequestBody CommandManagerSelf command) {
         int managerId = ProjectWebUtil.getManagerId();
         this.managerService.updateSelf(managerId, command);
