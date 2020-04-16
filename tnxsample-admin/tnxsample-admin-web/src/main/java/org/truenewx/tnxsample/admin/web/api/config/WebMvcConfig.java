48d3d8f0243e0d2dd46557e3a4dd2a3fb0b9d8b9
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-16 21:22:16 +0800

    解决axios的跨域访问问题

--- tnxsample-admin/tnxsample-admin-web/src/main/java/org/truenewx/tnxsample/admin/web/api/config/WebMvcConfig.java
+++ tnxsample-admin/tnxsample-admin-web/src/main/java/org/truenewx/tnxsample/admin/web/api/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package org.truenewx.tnxsample.admin.web.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.truenewx.tnxjee.web.config.WebMvcConfigurationSupport;
@@ -20,9 +21,11 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8888")
+                .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowCredentials(true);
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders(HttpHeaders.SET_COOKIE);
     }
 
 }
--- tnxsample-admin/tnxsample-admin-web/src/main/java/org/truenewx/tnxsample/admin/web/api/config/WebSecurityConfig.java
+++ tnxsample-admin/tnxsample-admin-web/src/main/java/org/truenewx/tnxsample/admin/web/api/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.truenewx.tnxsample.admin.web.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.truenewx.tnxjee.web.security.config.WebSecurityConfigurerSupport;
 
 import java.util.Collection;
@@ -15,6 +16,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerSupport {
         Collections.addAll(patterns, "/swagger-ui.html", "/webjars/**", "/v2/api-docs",
                 "/swagger-resources/**");
         return patterns;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.formLogin().defaultSuccessUrl("/", true);
     }
 
 }
