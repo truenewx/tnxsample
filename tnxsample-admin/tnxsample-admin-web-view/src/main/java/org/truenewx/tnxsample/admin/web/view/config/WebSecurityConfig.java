ed90c7bc84ea541bb39eadd03264fb8a9b7250cf
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-17 22:26:36 +0800

    使用属性配置视图层的安全配置

--- tnxsample-admin/tnxsample-admin-web-view/src/main/java/org/truenewx/tnxsample/admin/web/view/config/WebSecurityConfig.java
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/java/org/truenewx/tnxsample/admin/web/view/config/WebSecurityConfig.java
@@ -5,8 +5,5 @@ import org.truenewx.tnxjee.web.view.security.config.WebViewSecurityConfigurerSup
 
 @Configuration
 public class WebSecurityConfig extends WebViewSecurityConfigurerSupport {
-    @Override
-    protected boolean allowedCrossDomainRequest() {
-        return true;
-    }
+    
 }
--- tnxsample-admin/tnxsample-admin-web-view/src/main/java/org/truenewx/tnxsample/admin/web/view/controller/HeaderController.java
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/java/org/truenewx/tnxsample/admin/web/view/controller/HeaderController.java
@@ -1,9 +1,11 @@
 package org.truenewx.tnxsample.admin.web.view.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.truenewx.tnxjee.web.security.config.annotation.ConfigAnonymous;
+import org.truenewx.tnxsample.admin.service.ManagerService;
 import org.truenewx.tnxsample.admin.web.view.util.ProjectWebUtil;
 
 /**
@@ -11,12 +13,15 @@ import org.truenewx.tnxsample.admin.web.view.util.ProjectWebUtil;
  */
 @Controller
 public class HeaderController {
+    @Autowired
+    private ManagerService managerService;
 
     @RequestMapping("/header")
     @ConfigAnonymous
     public ModelAndView execute() {
         ModelAndView mav = new ModelAndView("/header");
-        mav.addObject("manager", ProjectWebUtil.getManagerDetails());
+        int managerId = ProjectWebUtil.getManagerId();
+        mav.addObject("manager", this.managerService.load(managerId));
         return mav;
     }
 
