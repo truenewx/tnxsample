51e862c3c7d3afa36bdd8cd60abfab32d83beec5
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-17 21:09:52 +0800

    使用属性配置资源映射

--- tnxsample-admin/tnxsample-admin-web-api/src/main/java/org/truenewx/tnxsample/admin/web/api/config/WebMvcConfig.java
+++ tnxsample-admin/tnxsample-admin-web-api/src/main/java/org/truenewx/tnxsample/admin/web/api/config/WebMvcConfig.java
@@ -1,19 +1,9 @@
 package org.truenewx.tnxsample.admin.web.api.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.truenewx.tnxjee.web.config.WebMvcConfigurationSupport;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        super.addResourceHandlers(registry);
-        registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 
 }
