576341bfcdf2301b0c6a3458800c7ff9779ff972
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-17 22:03:43 +0800

    使用属性配置api层的安全配置

--- tnxsample-admin/tnxsample-admin-web-api/src/main/java/org/truenewx/tnxsample/admin/web/api/config/WebSecurityConfig.java
+++ tnxsample-admin/tnxsample-admin-web-api/src/main/java/org/truenewx/tnxsample/admin/web/api/config/WebSecurityConfig.java
@@ -1,22 +1,11 @@
 package org.truenewx.tnxsample.admin.web.api.config;
 
-import java.util.Collection;
-import java.util.Collections;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.truenewx.tnxjee.web.security.config.WebSecurityConfigurerSupport;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerSupport {
-
-    @Override
-    protected Collection<String> getIgnoringAntPatterns() {
-        Collection<String> patterns = super.getIgnoringAntPatterns();
-        Collections.addAll(patterns, "/swagger-ui.html", "/webjars/**", "/v2/api-docs",
-                "/swagger-resources/**");
-        return patterns;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
