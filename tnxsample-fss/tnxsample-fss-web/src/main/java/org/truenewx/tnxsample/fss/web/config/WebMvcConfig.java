06bc0262ff4fadcf2b9880108760f554d019b35c
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-22 09:22:39 +0800

    fss工程能正常访问

---
+++ tnxsample-fss/tnxsample-fss-web/src/main/java/org/truenewx/tnxsample/fss/web/config/WebMvcConfig.java
@@ -0,0 +1,9 @@
+package org.truenewx.tnxsample.fss.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.truenewx.tnxjee.web.config.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+}
---
+++ tnxsample-fss/tnxsample-fss-web/src/main/java/org/truenewx/tnxsample/fss/web/config/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package org.truenewx.tnxsample.fss.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.truenewx.tnxjee.web.security.config.WebSecurityConfigurerSupport;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerSupport {
+
+}
