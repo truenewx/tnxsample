31d3eed3bee466553b26f845d990feeb409d4a87
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-24 21:33:09 +0800

    使用@ResponseStream标注解决响应为二进制流时的异常处理

--- tnxsample-fss/tnxsample-fss-web/src/main/java/org/truenewx/tnxsample/fss/web/config/WebMvcConfig.java
+++
@@ -1,9 +0,0 @@
-package org.truenewx.tnxsample.fss.web.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.truenewx.tnxjee.web.config.WebMvcConfigurationSupport;
-
-@Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
-
-}
--- tnxsample-fss/tnxsample-fss-web/src/main/java/org/truenewx/tnxsample/fss/web/config/WebSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package org.truenewx.tnxsample.fss.web.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.truenewx.tnxjee.web.security.config.WebSecurityConfigurerSupport;
-
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerSupport {
-
-}
