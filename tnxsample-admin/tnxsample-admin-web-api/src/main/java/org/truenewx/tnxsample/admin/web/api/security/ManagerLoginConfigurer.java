74ce6b86ef0e0ff74d091df93ee69a3ac96bd172
projavaer <pro_javaer@163.com>
2020-05-14 11:30:40 +0800

    解决ajax单点登录问题

--- tnxsample-admin/tnxsample-admin-web-api/src/main/java/org/truenewx/tnxsample/admin/web/api/security/ManagerLoginConfigurer.java
+++ tnxsample-admin/tnxsample-admin-web-api/src/main/java/org/truenewx/tnxsample/admin/web/api/security/ManagerLoginConfigurer.java
@@ -1,11 +1,14 @@
 package org.truenewx.tnxsample.admin.web.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.cas.authentication.CasAuthenticationProvider;
-import org.springframework.security.cas.web.CasAuthenticationFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.stereotype.Component;
+import org.truenewx.tnxjee.web.api.meta.model.ApiMetaProperties;
 import org.truenewx.tnxjee.web.security.config.SecurityLoginConfigurerSupport;
+import org.truenewx.tnxjeex.cas.client.filter.CasClientAuthenticationFilter;
 
 /**
  * 管理员登录安全配置器
@@ -15,10 +18,17 @@ import org.truenewx.tnxjee.web.security.config.SecurityLoginConfigurerSupport;
 public class ManagerLoginConfigurer
         extends SecurityLoginConfigurerSupport<CasAuthenticationProvider> {
 
+    @Autowired(required = false)
+    private ApiMetaProperties apiMetaProperties;
+    @Autowired
+    private RedirectStrategy redirectStrategy;
+
     @Override
     // 指定登录授权过滤器实例，如果框架中现有的不满足需要，需自定义一个
     public void configure(HttpSecurity http) throws Exception {
-        CasAuthenticationFilter filter = new CasAuthenticationFilter();
+        CasClientAuthenticationFilter filter = new CasClientAuthenticationFilter();
+        filter.setRedirectStrategy(this.redirectStrategy);
+        filter.setSuccessTargetUrlParameter(this.apiMetaProperties.getLoginSuccessRedirectParameter());
         filter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class)); // 固定必须
         http.addFilterAt(filter, filter.getClass());
     }
