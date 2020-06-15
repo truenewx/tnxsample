15ab719008ea19a42bd85c3271c6ce8d21c08a05
projavaer <pro_javaer@163.com>
2020-06-15 17:59:44 +0800

    cli工程build至view工程的static目录

--- tnxsample-admin/tnxsample-admin-web-cli/src/App.vue
+++ tnxsample-admin/tnxsample-admin-web-cli/src/App.vue
@@ -1,29 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <a-button type="primary">Hello World</a-button>
-  </div>
+    <a-locale-provider :locale="locale">
+        <div id="app">
+            <img alt="Vue logo" src="./assets/logo.png">
+            <p>
+                <a-button type="primary">Hello World</a-button>
+            </p>
+        </div>
+    </a-locale-provider>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN';
+    import moment from 'moment';
+    import 'moment/locale/zh-cn';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    moment.locale('zh-cn');
+
+    export default {
+        name: 'App',
+        components: {
+        },
+        data: function() {
+            return {
+                locale: zh_CN
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
