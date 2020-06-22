ee6624298e3b2471a499e3265c6daa72b5cd8f41
projavaer <pro_javaer@163.com>
2020-06-22 09:09:38 +0800

    布局模块抽取至layout目录

--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/App.vue
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/App.vue
@@ -1,33 +1,28 @@
 <template>
     <div id="app" class="d-flex flex-column">
         <el-header height="auto" class="border-bottom fixed-top">
-            <page-header/>
+            <layout-header/>
         </el-header>
-        <div class="flex-grow-1 d-flex page-container">
-            <el-aside width="auto">
-                <page-menu/>
-            </el-aside>
-            <el-main class="flex-grow-1 border-left">Main</el-main>
-        </div>
+        <layout-router/>
         <el-footer height="auto" class="border-top py-2">
-            <page-footer/>
+            <layout-footer/>
         </el-footer>
     </div>
 </template>
 
 <script>
-    import Header from './pages/Header.vue';
-    import Menu from './pages/Menu.vue';
-    import Footer from './pages/Footer.vue';
+    import header from './layout/header.vue';
+    import router from './layout/router.vue';
+    import footer from './layout/footer.vue';
 
     export default {
         name: 'App',
         components: {
-            "page-header": Header,
-            "page-menu": Menu,
-            "page-footer": Footer,
+            "layout-header": header,
+            "layout-router": router,
+            "layout-footer": footer,
         },
-        data () {
+        data() {
             return {}
         }
     }
@@ -46,14 +41,5 @@
     header {
         line-height: 55px;
         background-color: #fff;
-    }
-
-    aside {
-        min-width: 222px;
-        height: 100%;
-    }
-
-    .page-container {
-        padding-top: 56px;
     }
 </style>
--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/main.js
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import ElementUI from 'element-ui';
 import './assets/bootstrap-base.css';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -9,7 +8,6 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-Vue.use(VueRouter);
 Vue.use(ElementUI);
 
 new Vue({
