58331a1ddc11c972261c053731fec9d32f8f9ad6
projavaer <pro_javaer@163.com>
2020-06-22 13:48:26 +0800

    根据菜单获取路由的方法移至路由配置中

--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/layout/menu.config.js
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/layout/menu.config.js
@@ -1,48 +1,26 @@
 // menu.config.js
-const items = [{
-    caption: '系统管理',
-    icon: 'fa-cogs',
-    subs: [{
-        caption: '角色管理',
-        icon: 'fa-id-badge',
-        path: '/role/list',
-    }, {
-        caption: '管理员管理',
-        icon: 'fa-users-cog',
-        path: '/manager/list',
-    }, {
-        caption: '操作日志',
-        path: '/log/action',
-    }, {
-        caption: '系统日志',
-        path: '/log/system',
-    }]
-}, {
-    caption: '客户管理',
-    icon: 'fa-user',
-    path: '/customer/list',
-}];
-
-function applyItemsToRoutes(items, routes) {
-    if (items && items.length) {
-        items.forEach(item => {
-            if (item.path) {
-                routes.push({
-                    path: item.path,
-                    component: () => import('../pages' + item.path + '.vue')
-                });
-            } else {
-                applyItemsToRoutes(item.subs, routes);
-            }
-        });
-    }
-}
-
 export default {
-    items: items,
-    getRoutes: () => {
-        const routes = [];
-        applyItemsToRoutes(items, routes);
-        return routes;
-    }
+    items: [{
+        caption: '系统管理',
+        icon: 'fa-cogs',
+        subs: [{
+            caption: '角色管理',
+            icon: 'fa-id-badge',
+            path: '/role/list',
+        }, {
+            caption: '管理员管理',
+            icon: 'fa-users-cog',
+            path: '/manager/list',
+        }, {
+            caption: '操作日志',
+            path: '/log/action',
+        }, {
+            caption: '系统日志',
+            path: '/log/system',
+        }]
+    }, {
+        caption: '客户管理',
+        icon: 'fa-user',
+        path: '/customer/list',
+    }]
 };
--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/router.config.js
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/router.config.js
@@ -1,5 +1,26 @@
 import menuConfig from './layout/menu.config';
 
+function applyItemsToRoutes (items, routes) {
+    if (items && items.length) {
+        items.forEach(item => {
+            if (item.path) {
+                routes.push({
+                    path: item.path,
+                    component: () => import('./pages' + item.path + '.vue')
+                });
+            } else {
+                applyItemsToRoutes(item.subs, routes);
+            }
+        });
+    }
+}
+
+function getRoutes () {
+    const routes = [];
+    applyItemsToRoutes(menuConfig.items, routes);
+    return routes;
+}
+
 export default {
-    routes: menuConfig.getRoutes()
+    routes: getRoutes()
 };
