67bb47bcc9524e999bea867aa18583b2d109d72d
projavaer <pro_javaer@163.com>
2020-06-24 17:00:26 +0800

    bootstrap-base和tnxel移至tnxweb组件库中

--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/main.js
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/main.js
@@ -2,15 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routerConfig from './router.config.js';
 import ElementUI from 'element-ui';
+import '@truenewx/tnxweb/packages/bootstrap/base-4.5.0.css';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import tnx from '@truenewx/tnxweb';
-import './assets/tnxel.css';
+import '@truenewx/tnxweb/packages/element/tnxel.css';
 import './assets/app.css';
 import App from './App.vue';
 
-
-console.info(tnx.context);
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
