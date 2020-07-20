991314f6d549820291c1d12ed53a31b67f72c579
projavaer <pro_javaer@163.com>
2020-07-20 15:27:21 +0800

    修正开发模式下无法正常显示的问题

--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/vue.config.js
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
     publicPath: './',
     outputDir: '../webapp/static',
     assetsDir: 'assets',
+    indexPath: 'main.html',
     devServer: {
         port: 8087
     },
@@ -10,7 +11,7 @@ module.exports = {
         index: {
             entry: 'src/main.js',
             template: 'public/index.html',
-            filename: 'main.html',
+            filename: 'index.html', // 不能修改为其它值，否则页面无法正常打开，构建时被上面的indexPath覆盖
             minify: false, // 不压缩html代码
         }
     },
