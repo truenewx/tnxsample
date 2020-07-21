a4f62f7f9224628c0b0e5eb6b7d288a002ebf18c
projavaer <pro_javaer@163.com>
2020-07-21 17:11:27 +0800

    添加路径权限判断

--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/pages/role/list.vue
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/pages/role/list.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <p>角色管理</p>
-        <el-button type="primary" @click="toAdd">添加角色</el-button>
+        <el-button type="primary" @click="toAdd" v-if="addable">添加角色</el-button>
     </div>
 </template>
 
 <script>
+    import menu from '../../layout/menu.js';
+
     export default {
         title: '角色清单',
         data () {
-            return {};
+            return {
+                addable: menu.isGranted('/role/add'),
+            };
         },
         methods: {
             toAdd () {
