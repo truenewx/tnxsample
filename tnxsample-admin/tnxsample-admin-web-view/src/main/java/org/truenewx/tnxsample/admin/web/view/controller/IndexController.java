2122ae578aab5285319b4b0acbadfe22cbad45eb
jianglei81 <jianglei81@sina.cn>
2020-08-06 16:17:46 +0800

    配置更新

--- tnxsample-admin/tnxsample-admin-web-view/src/main/java/org/truenewx/tnxsample/admin/web/view/controller/IndexController.java
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/java/org/truenewx/tnxsample/admin/web/view/controller/IndexController.java
@@ -1,10 +1,8 @@
 package org.truenewx.tnxsample.admin.web.view.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.truenewx.tnxjee.web.security.config.annotation.ConfigAuthority;
-import org.truenewx.tnxjee.web.view.menu.UserMenuResolver;
 
 /**
  * 首页
@@ -13,9 +11,6 @@ import org.truenewx.tnxjee.web.view.menu.UserMenuResolver;
  */
 @Controller
 public class IndexController {
-
-    @Autowired
-    private UserMenuResolver menuResolver;
 
     @RequestMapping("/")
     @ConfigAuthority
