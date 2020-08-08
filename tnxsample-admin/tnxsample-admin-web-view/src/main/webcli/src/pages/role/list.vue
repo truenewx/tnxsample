e3c3d7d3f8791776a7510d7f3903e0957afc7917
jianglei <jianglei81@sina.cn>
2020-08-08 21:52:30 +0800

    引入表格

--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/pages/role/list.vue
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/pages/role/list.vue
@@ -1,24 +1,49 @@
 <template>
     <div>
-        <p>角色管理</p>
-        <el-button type="primary" @click="toAdd" v-if="addable">添加角色</el-button>
+        <el-button type="primary" class="mb-3" @click="toAdd" v-if="addable">添加角色</el-button>
+        <el-table :data="records" current-row-key="role" border stripe>
+            <el-table-column prop="name" label="角色名称"></el-table-column>
+            <el-table-column label="管理员数量">
+                <template slot-scope="scope">
+                    <el-button type="text">{{scope.row.managerNum}}</el-button>
+                </template>
+            </el-table-column>
+            <el-table-column fixed="right" label="操作">
+                <template slot-scope="scope">
+                    <el-button type="text">查看</el-button>
+                    <el-button type="text">编辑</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <script>
-    import menu from '../../layout/menu.js';
+import menu from '../../layout/menu.js';
 
-    export default {
-        title: '角色清单',
-        data () {
-            return {
-                addable: menu.isGranted('/role/add'),
-            };
-        },
-        methods: {
-            toAdd () {
-                this.$router.push('/role/add');
-            }
+export default {
+    data () {
+        return {
+            addable: menu.isGranted('/role/add'),
+            records: [{
+                name: '王小虎',
+                managerNum: 0,
+            }, {
+                name: '王小虎',
+                managerNum: 1,
+            }, {
+                name: '王小虎',
+                managerNum: 2,
+            }, {
+                name: '王小虎',
+                managerNum: 3,
+            }],
+        };
+    },
+    methods: {
+        toAdd () {
+            this.$router.push('/role/add');
         }
     }
+}
 </script>
