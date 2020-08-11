208263193a04d0c5d50cd56e7b1dc7ca39a71c49
jianglei81 <jianglei81@sina.cn>
2020-08-11 17:30:43 +0800

    添加客户端格式校验

--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/pages/self/info.vue
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/pages/self/info.vue
@@ -1,5 +1,6 @@
 <template>
-    <el-form label-position="right" label-width="auto" :model="model" :rules="rules" status-icon>
+    <el-form label-position="right" label-width="auto" ref="form" :model="model" :rules="rules"
+        status-icon>
         <el-form-item label="用户名">{{model.username}}</el-form-item>
         <el-form-item label="是否超管">
             <div>
@@ -20,65 +21,71 @@
 </template>
 
 <script>
-    import {app, tnx, util} from '../../app.js';
+import {app, tnx, util} from '../../app.js';
 
-    export default {
-        props: ['opener'],
-        data () {
-            return {
-                model: {
-                    fullName: '',
-                    headImageFile: null,
-                },
-                rules: {},
-            };
-        },
-        created () {
-            tnx.showLoading();
-            const beginTime = new Date().getTime();
-            const vm = this;
-            app.rpc.get('/manager/self/info', model => {
-                vm.model = model;
-                // 先给模型赋值，再加载元数据，以确保字段校验不提前进行而导致失败
-                app.rpc.getMeta('/manager/self/info', meta => {
-                    vm.rules = meta.rules;
-                    util.setMinTimeout(beginTime, function() {
-                        tnx.closeLoading();
-                    }, 500);
-                });
-            });
-        },
-        methods: {
-            dialog () {
-                return {
-                    title: '修改个人信息',
-                    width: '25%',
-                    type: 'confirm',
-                    click: this.toSubmit,
-                }
+export default {
+    props: ['opener'],
+    data() {
+        return {
+            model: {
+                fullName: '',
+                headImageFile: null,
             },
-            toSubmit (yes, close) {
-                if (yes) {
-                    const model = this.model;
-                    const opener = this.opener;
-                    tnx.showLoading();
-                    this.$refs.headImageUpload.getStorageUrls().then(function(storageUrls) {
-                        model.headImageUrl = storageUrls[0];
-                        const beginTime = new Date().getTime();
-                        app.rpc.post('/manager/self/info', model, function() {
-                            opener.managerCaption = model.fullName;
-                            util.setMinTimeout(beginTime, function() {
-                                tnx.toast('修改成功', () => {
-                                    close();
-                                });
-                            }, 500);
+            rules: {},
+        };
+    },
+    created() {
+        tnx.showLoading();
+        const beginTime = new Date().getTime();
+        const vm = this;
+        app.rpc.get('/manager/self/info', model => {
+            vm.model = model;
+            // 先给模型赋值，再加载元数据，以确保字段校验不提前进行而导致失败
+            app.rpc.getMeta('/manager/self/info', meta => {
+                vm.rules = meta.rules;
+                util.setMinTimeout(beginTime, function() {
+                    tnx.closeLoading();
+                }, 500);
+            });
+        });
+    },
+    methods: {
+        dialog() {
+            return {
+                title: '修改个人信息',
+                width: '25%',
+                type: 'confirm',
+                click: this.toSubmit,
+            }
+        },
+        toSubmit(yes, close) {
+            if (yes) {
+                const form = this.$refs.form;
+                const vm = this;
+                form.validate(function(success) {
+                    if (success) {
+                        const model = vm.model;
+                        const opener = vm.opener;
+                        tnx.showLoading();
+                        vm.$refs.headImageUpload.getStorageUrls().then(function(storageUrls) {
+                            model.headImageUrl = storageUrls[0];
+                            const beginTime = new Date().getTime();
+                            app.rpc.post('/manager/self/info', model, function() {
+                                opener.managerCaption = model.fullName;
+                                util.setMinTimeout(beginTime, function() {
+                                    tnx.toast('修改成功', () => {
+                                        close();
+                                    });
+                                }, 500);
+                            });
+                        }).catch(function(file) {
+                            tnx.alert('文件"' + file.name + '"还未上传完毕，请稍候');
                         });
-                    }).catch(function(file) {
-                        tnx.alert('文件"' + file.name + '"还未上传完毕，请稍候');
-                    });
-                    return false;
-                }
+                    }
+                });
+                return false; // 不立即关闭对话框
             }
         }
     }
+}
 </script>
