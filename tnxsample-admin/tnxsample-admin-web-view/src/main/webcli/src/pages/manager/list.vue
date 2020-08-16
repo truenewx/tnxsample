d3213f2b44c21b80a4fba804eaf11fff47c9af78
jianglei <jianglei81@sina.cn>
2020-08-16 21:38:56 +0800

    管理员管理ing

--- tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/pages/manager/list.vue
+++ tnxsample-admin/tnxsample-admin-web-view/src/main/webcli/src/pages/manager/list.vue
@@ -4,32 +4,30 @@
             <el-button type="primary" @click="toAdd" v-if="addable">添加管理员</el-button>
         </div>
         <el-table :data="records" :empty-text="emptyRecordText" border stripe>
-            <el-table-column prop="name" label="名称" min-width="160px" width="160px"/>
-            <el-table-column label="操作权限" class-name="tnxel-table_tags nowrap" min-width="40%">
+            <el-table-column prop="jobNo" label="工号" min-width="60px"/>
+            <el-table-column prop="fullName" label="姓名" min-width="80px"/>
+            <el-table-column prop="username" label="用户名" min-width="100px"/>
+            <el-table-column label="所属角色" class-name="tnxel-table_tags nowrap" min-width="40%">
                 <template slot-scope="scope">
-                    <el-tag v-for="permission in scope.row.permissions" :key="permission">
-                        {{permission}}
-                    </el-tag>
-                </template>
-            </el-table-column>
-            <el-table-column label="管理员" class-name="tnxel-table_tags nowrap" min-width="40%">
-                <template slot-scope="scope">
-                    <template v-if="scope.row.managerNum > 0">
-                        <span class="mr-2">共{{scope.row.managerNum}}人</span>
-                        <el-tag type="info" v-for="manager in scope.row.managers" :key="manager">
-                            {{manager}}
+                    <template v-if="scope.row.roles.length > 0">
+                        <el-tag type="info" v-for="role in scope.row.roles" :key="role.id">
+                            {{role.name}}
                         </el-tag>
                     </template>
                     <span class="text-muted" v-else>&lt;无&gt;</span>
                 </template>
             </el-table-column>
+            <el-table-column label="状态" min-width="50px">
+                <template slot-scope="scope">
+                    <i class="el-icon-remove link" v-if="scope.row.disabled"></i>
+                    <i class="el-icon-success link" v-else></i>
+                </template>
+            </el-table-column>
             <el-table-column label="操作" class-name="tnxel-table_tags" min-width="100px"
                 width="100px" header-align="center" align="center">
                 <template slot-scope="scope">
-                    <router-link :to="'/role/' + scope.row.id + '/update'"
+                    <router-link :to="'/manager/' + scope.row.id + '/update'"
                         class="tnxel-table_tag" v-if="updatable">修改</router-link>
-                    <a href="javascript:void(0)" class="tnxel-table_tag" @click="toDelete($event)"
-                        v-if="deletable" :data-index="scope.$index">删除</a>
                 </template>
             </el-table-column>
         </el-table>
@@ -37,16 +35,16 @@
 </template>
 
 <script>
-import menu from "@/layout/menu";
-import {$, app, tnx} from "@/app";
+import menu from "../../layout/menu";
+import {app} from "../../app";
 
 export default {
     data() {
         return {
             addable: false,
             updatable: false,
-            deletable: false,
             records: null,
+            paging: null,
         };
     },
     computed: {
@@ -57,43 +55,18 @@ export default {
     created() {
         const vm = this;
         menu.loadGrantedItems(() => {
-            vm.addable = menu.isGranted('/role/add');
-            vm.updatable = menu.isGranted('/role/*/update');
-            vm.deletable = menu.isGranted('/role/*/delete');
+            vm.addable = menu.isGranted('/manager/add');
+            vm.updatable = menu.isGranted('/manager/*/update');
+            vm.deletable = menu.isGranted('/manager/*/delete');
         });
-        app.rpc.get('/role/list', {}, function(records) {
-            for (let record of records) {
-                if (record.permissions) {
-                    for (let i = 0; i < record.permissions.length; i++) {
-                        const item = menu.getItemByPermission(record.permissions[i]);
-                        if (item) {
-                            record.permissions[i] = item.caption;
-                        }
-                    }
-                }
-            }
-            vm.records = records;
+        app.rpc.get('/manager/list', {}, function(result) {
+            vm.records = result.records;
+            vm.paging = result.paging;
         });
     },
     methods: {
         toAdd() {
-            this.$router.push('/role/add');
-        },
-        toDelete(event) {
-            const index = parseInt($(event.target).attr('data-index'));
-            const roles = this.records;
-            const role = roles[index];
-            let message = '确定要删除吗？';
-            if (role.managerNum > 0) {
-                message = '删除角色将使得其下的所有管理员（共' + role.managerNum + '人）失去该角色及其操作权限，' + message;
-            }
-            tnx.confirm(message, yes => {
-                if (yes) {
-                    app.rpc.post('/role/' + role.id + '/delete', () => {
-                        roles.splice(index, 1);
-                    });
-                }
-            });
+            this.$router.push('/manager/add');
         }
     }
 }
