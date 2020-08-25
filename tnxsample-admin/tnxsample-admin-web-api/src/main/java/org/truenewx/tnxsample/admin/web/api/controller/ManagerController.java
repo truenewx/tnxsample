4ed3198f97d0d52231430ab2166366fdd8d14e5d
jianglei81 <jianglei81@sina.cn>
2020-08-25 17:52:47 +0800

    消除警告

--- tnxsample-admin/tnxsample-admin-web-api/src/main/java/org/truenewx/tnxsample/admin/web/api/controller/ManagerController.java
+++ tnxsample-admin/tnxsample-admin-web-api/src/main/java/org/truenewx/tnxsample/admin/web/api/controller/ManagerController.java
@@ -1,14 +1,19 @@
 package org.truenewx.tnxsample.admin.web.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.truenewx.tnxjee.model.query.QueryResult;
 import org.truenewx.tnxjee.web.http.annotation.ResultFilter;
 import org.truenewx.tnxjee.web.security.config.annotation.ConfigAuthority;
 import org.truenewx.tnxsample.admin.model.entity.Manager;
 import org.truenewx.tnxsample.admin.model.entity.Role;
 import org.truenewx.tnxsample.admin.service.ManagerService;
-import org.truenewx.tnxsample.admin.service.RoleService;
 import org.truenewx.tnxsample.admin.service.model.ManagerCommand;
 import org.truenewx.tnxsample.common.CommonConstants;
 
@@ -21,12 +26,9 @@ public class ManagerController {
 
     @Autowired
     private ManagerService managerService;
-    @Autowired
-    private RoleService roleService;
 
     @GetMapping("/list")
-    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER,
-            rank = CommonConstants.MANAGER_RANK_TOP)
+    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER, rank = CommonConstants.MANAGER_RANK_TOP)
     @ResultFilter(type = Role.class, included = { "id", "name" })
     public QueryResult<Manager> list(
             @RequestParam(name = "keyword", required = false) String keyword,
@@ -36,38 +38,33 @@ public class ManagerController {
     }
 
     @PostMapping("/add")
-    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER,
-            rank = CommonConstants.MANAGER_RANK_TOP)
+    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER, rank = CommonConstants.MANAGER_RANK_TOP)
     public void add(@RequestBody ManagerCommand command) {
         this.managerService.add(command);
     }
 
     @GetMapping("/{id}")
-    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER,
-            rank = CommonConstants.MANAGER_RANK_TOP)
+    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER, rank = CommonConstants.MANAGER_RANK_TOP)
     @ResultFilter(type = Role.class, included = { "id", "name" })
     public Manager detail(@PathVariable("id") int id) {
         return this.managerService.load(id);
     }
 
     @PostMapping("/{id}/update")
-    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER,
-            rank = CommonConstants.MANAGER_RANK_TOP)
+    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER, rank = CommonConstants.MANAGER_RANK_TOP)
     public void update(@PathVariable("id") int id, @RequestBody ManagerCommand command) {
         this.managerService.update(id, command);
     }
 
     @PostMapping("/{id}/update-disabled")
-    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER,
-            rank = CommonConstants.MANAGER_RANK_TOP)
+    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER, rank = CommonConstants.MANAGER_RANK_TOP)
     public void updateDisabled(@PathVariable("id") int id,
             @RequestParam("disabled") boolean disabled) {
         this.managerService.updateDisabled(id, disabled);
     }
 
     @PostMapping("/{id}/reset-password")
-    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER,
-            rank = CommonConstants.MANAGER_RANK_TOP)
+    @ConfigAuthority(type = CommonConstants.USER_TYPE_MANAGER, rank = CommonConstants.MANAGER_RANK_TOP)
     public void resetPassword(@PathVariable("id") int id,
             @RequestParam("password") String password) {
         this.managerService.resetPassword(id, password);
