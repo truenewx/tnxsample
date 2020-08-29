c62d81513c02ad50e5d31d110ed2cb60b26c0adb
jianglei <jianglei81@sina.cn>
2020-08-29 08:58:08 +0800

    使用lombok

--- tnxsample-admin/tnxsample-admin-service/src/main/java/org/truenewx/tnxsample/admin/service/model/RoleCommand.java
+++ tnxsample-admin/tnxsample-admin-service/src/main/java/org/truenewx/tnxsample/admin/service/model/RoleCommand.java
@@ -3,48 +3,21 @@ package org.truenewx.tnxsample.admin.service.model;
 import org.truenewx.tnxjee.model.CommandModel;
 import org.truenewx.tnxsample.admin.model.entity.Role;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 角色命令模型
  *
  * @author jianglei
  */
+@Getter
+@Setter
 public class RoleCommand implements CommandModel<Role> {
 
     private String name;
     private String remark;
     private String[] permissions;
     private int[] managerIds;
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public String getRemark() {
-        return this.remark;
-    }
-
-    public void setRemark(final String remark) {
-        this.remark = remark;
-    }
-
-    public String[] getPermissions() {
-        return this.permissions;
-    }
-
-    public void setPermissions(final String[] permissions) {
-        this.permissions = permissions;
-    }
-
-    public int[] getManagerIds() {
-        return this.managerIds;
-    }
-
-    public void setManagerIds(final int[] managerIds) {
-        this.managerIds = managerIds;
-    }
 
 }
