47299ddcd7a26b436391fba30eace67fce644f82
jianglei <jianglei81@sina.cn>
2020-11-20 14:47:42 +0800

    改进多登录方式的支持

--- tnxsample-cas-web/src/main/java/org/truenewx/tnxsample/cas/web/authentication/UserLoginAuthenticator.java
+++ tnxsample-cas-web/src/main/java/org/truenewx/tnxsample/cas/web/authentication/UserLoginAuthenticator.java
@@ -1,15 +1,16 @@
 package org.truenewx.tnxsample.cas.web.authentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
 import org.truenewx.tnxjee.model.spec.user.security.UserSpecificDetails;
-import org.truenewx.tnxjee.service.spec.user.UserPasswordLoginValidator;
+import org.truenewx.tnxjeex.cas.server.security.authentication.CasServerLoginAuthenticator;
 import org.truenewx.tnxsample.cas.web.rpc.CustomerLoginClient;
 import org.truenewx.tnxsample.cas.web.rpc.ManagerLoginClient;
 import org.truenewx.tnxsample.common.constant.UserTypes;
 
-@Service
-public class UserLoginValidator implements UserPasswordLoginValidator {
+@Component
+public class UserLoginAuthenticator implements CasServerLoginAuthenticator<UsernamePasswordAuthenticationToken> {
 
     @Autowired
     private ManagerLoginClient managerLoginClient;
@@ -17,8 +18,10 @@ public class UserLoginValidator implements UserPasswordLoginValidator {
     private CustomerLoginClient customerLoginClient;
 
     @Override
-    public UserSpecificDetails<?> validatePasswordLogin(String userType, String scope, String username,
-            String password) {
+    public UserSpecificDetails<?> authenticate(String userType, String scope,
+            UsernamePasswordAuthenticationToken token) {
+        String username = (String) token.getPrincipal();
+        String password = (String) token.getCredentials();
         switch (userType) {
             case UserTypes.MANAGER:
                 return this.managerLoginClient.validate(username, password);
