57fd50e404341c2a18332b029760514cbe9d8014
jianglei <jianglei81@sina.cn>
2020-12-25 22:19:31 +0800

    解决CAS登录模式下多应用同时登录，但不同应用的登录用户数据存在差异的支持

--- tnxsample-cas-web/src/main/java/org/truenewx/tnxsample/cas/web/controller/LoginController.java
+++ tnxsample-cas-web/src/main/java/org/truenewx/tnxsample/cas/web/controller/LoginController.java
@@ -1,10 +1,10 @@
 package org.truenewx.tnxsample.cas.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.truenewx.tnxjeex.cas.server.controller.CasLoginControllerSupport;
+import org.truenewx.tnxjeex.cas.server.controller.CasServerLoginControllerSupport;
 
 @Controller
-public class LoginController extends CasLoginControllerSupport {
+public class LoginController extends CasServerLoginControllerSupport {
 
     @Override
     protected String getDefaultService() {
