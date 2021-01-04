f379b3c3c6af3f9e1f635621c0dca9e12f877806
jianglei <jianglei81@sina.cn>
2021-01-04 21:22:40 +0800

    服务票据更改为应用票据，使用CAS安全地址提供者

--- tnxsample-fss-web/src/main/java/org/truenewx/tnxsample/fss/web/config/WebSecurityConfig.java
+++ tnxsample-fss-web/src/main/java/org/truenewx/tnxsample/fss/web/config/WebSecurityConfig.java
@@ -1,23 +1,8 @@
 package org.truenewx.tnxsample.fss.web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.truenewx.tnxjee.webmvc.security.config.annotation.web.configuration.WebMvcSecurityConfigurerSupport;
-import org.truenewx.tnxjeex.cas.client.config.CasClientProperties;
 
 @Configuration
 public class WebSecurityConfig extends WebMvcSecurityConfigurerSupport {
-
-    @Autowired
-    private CasClientProperties casClientProperties;
-
-    @Override
-    public String getLoginFormUrl() {
-        return this.casClientProperties.getLoginFormUrl();
-    }
-
-    @Override
-    protected String getLogoutSuccessUrl() {
-        return this.casClientProperties.getLogoutProcessUrl(); // 本地登出后跳转到CAS服务端执行登出
-    }
 }
--- tnxsample-root/tnxsample-root-web/src/main/java/org/truenewx/tnxsample/root/web/config/WebSecurityConfig.java
+++ tnxsample-root/tnxsample-root-web/src/main/java/org/truenewx/tnxsample/root/web/config/WebSecurityConfig.java
@@ -1,24 +1,8 @@
 package org.truenewx.tnxsample.root.web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.truenewx.tnxjee.webmvc.view.security.config.WebViewSecurityConfigurerSupport;
-import org.truenewx.tnxjeex.cas.client.config.CasClientProperties;
 
 @Configuration
 public class WebSecurityConfig extends WebViewSecurityConfigurerSupport {
-
-    @Autowired
-    private CasClientProperties casClientProperties;
-
-    @Override
-    public String getLoginFormUrl() {
-        return this.casClientProperties.getLoginFormUrl();
-    }
-
-    @Override
-    protected String getLogoutSuccessUrl() {
-        return this.casClientProperties.getLogoutProcessUrl(); // 本地登出后跳转到CAS服务端执行登出
-    }
-
 }
