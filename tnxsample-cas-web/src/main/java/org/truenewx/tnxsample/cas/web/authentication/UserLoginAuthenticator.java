3a31471397e2fd7f9ffa65b63b50262cddf1daeb
jianglei <jianglei81@sina.cn>
2021-01-24 14:14:07 +0800

    去掉应用配置中的用户类型

--- tnxsample-cas-web/src/main/java/org/truenewx/tnxsample/cas/web/authentication/UserLoginAuthenticator.java
+++ tnxsample-cas-web/src/main/java/org/truenewx/tnxsample/cas/web/authentication/UserLoginAuthenticator.java
@@ -7,7 +7,7 @@ import org.truenewx.tnxjee.model.spec.user.security.UserSpecificDetails;
 import org.truenewx.tnxjeex.cas.server.security.authentication.CasServerLoginAuthenticator;
 import org.truenewx.tnxsample.cas.web.rpc.CustomerLoginClient;
 import org.truenewx.tnxsample.cas.web.rpc.ManagerLoginClient;
-import org.truenewx.tnxsample.common.constant.UserTypes;
+import org.truenewx.tnxsample.common.constant.AppNames;
 
 @Component
 public class UserLoginAuthenticator implements CasServerLoginAuthenticator<UsernamePasswordAuthenticationToken> {
@@ -18,14 +18,14 @@ public class UserLoginAuthenticator implements CasServerLoginAuthenticator<Usern
     private CustomerLoginClient customerLoginClient;
 
     @Override
-    public UserSpecificDetails<?> authenticate(String userType, String scope,
+    public UserSpecificDetails<?> authenticate(String appName, String scope,
             UsernamePasswordAuthenticationToken token) {
         String username = (String) token.getPrincipal();
         String password = (String) token.getCredentials();
-        switch (userType) {
-            case UserTypes.MANAGER:
+        switch (appName) {
+            case AppNames.ADMIN:
                 return this.managerLoginClient.validate(username, password);
-            case UserTypes.CUSTOMER:
+            case AppNames.ROOT:
                 return this.customerLoginClient.validate(username, password);
         }
         return null;
